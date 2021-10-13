36507ef3db894faca40cf36c274177b9d7ef389e
Ninad-Chaudhari <ninadchaudharioffice@gmail.com>
2021-10-13 19:12:56 +0530

    Made changes to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 import os
 
+PATH_ROOT = "/root/"
+PATH_IMAGES ="/root/images"
 os.system("pip install tensorflow==1.13.2")
 os.system("pip install tf_slim")
 os.system("pip show tensorflow")
+os.system("cd " + PATH_ROOT)
+os.system("git clone https://github.com/iotiotdotin/tf.git")
+os.mkdir(PATH_IMAGES)
