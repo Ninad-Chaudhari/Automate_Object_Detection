6102f1f01d1998a10e2eba34209b2e8b0061ba79
Ninad-Chaudhari <ninadchaudharioffice@gmail.com>
2021-10-18 22:15:50 +0530

    Added prepare dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,6 +1,10 @@
 import os
 import shutil
 import argparse
+import glob
+import urllib
+import tarfile
+from requests import get
 
 PATH_ROOT = os.getcwd()
 
@@ -26,3 +30,28 @@ output_path = PATH_ROOT + "/tfrecords"
 csv_path = PATH_ROOT + "/tfrecords/train.csv"                    
 
 os.system("python "+ PATH_ROOT+"/tf/research/generate_tfrecord.py -f " + file_path +" -i "+ images_path + " -o "+output_path+" -c "+csv_path+" -s "+args.split)
+
+# Downloading pretrained model
+os.chdir(PATH_ROOT + "/tf")
+MODEL = 'ssd_mobilenet_v1_coco_2017_11_17'
+MODEL_FILE = MODEL + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+DEST_DIR = 'pretrained_model'
+
+if not (os.path.exists(MODEL_FILE)):
+  with open(MODEL_FILE, "wb") as file:
+    # get request
+    response = get(DOWNLOAD_BASE + MODEL_FILE)
+    # write to file
+    file.write(response.content)
+    #opener = urllib.URLopener()
+    #opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+
+tar = tarfile.open(MODEL_FILE)
+tar.extractall()
+tar.close()
+
+os.remove(MODEL_FILE)
+if (os.path.exists(DEST_DIR)):
+  shutil.rmtree(DEST_DIR)
+os.rename(MODEL, DEST_DIR)
