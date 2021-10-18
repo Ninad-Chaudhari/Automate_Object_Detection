9fe216b6e66432529578413061ee834072e90791
Ninad-Chaudhari <ninadchaudharioffice@gmail.com>
2021-10-18 22:18:42 +0530

    Added prepare dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -55,3 +55,4 @@ os.remove(MODEL_FILE)
 if (os.path.exists(DEST_DIR)):
   shutil.rmtree(DEST_DIR)
 os.rename(MODEL, DEST_DIR)
+shutil.move(PATH_ROOT+"/tf/research/object_detection/samples/configs/ssd_mobilenet_v1_pets.config", PATH_ROOT +"/tf") 
